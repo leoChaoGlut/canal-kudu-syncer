4ce7a55bf0e60d859aa9e0b5db3e6885750b9ee5
leo <leoiscoding@163.com>
2020-10-22 09:21:31 +0800

    1.完成zk选举

--- server/src/main/java/personal/leo/cks/server/zk/ZkService.java
+++ server/src/main/java/personal/leo/cks/server/zk/ZkService.java
@@ -3,7 +3,10 @@ package personal.leo.cks.server.zk;
 import com.alibaba.otter.canal.common.zookeeper.ZkClientx;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.I0Itec.zkclient.IZkStateListener;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.data.Stat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,13 +15,11 @@ import personal.leo.cks.server.constants.PropKey;
 
 import javax.annotation.PostConstruct;
 import java.net.InetAddress;
-import java.util.List;
 
 @Slf4j
 @Component
 public class ZkService {
-    //    @Autowired
-//    ZooKeeper zooKeeper;
+
     @Autowired
     ZkClientx zkClientx;
     @Value("${server.port}")
@@ -40,26 +41,63 @@ public class ZkService {
         thisServerZkPath = availablePath + "/" + thisServer;
 
         tryToBuildZkFileStructure();
+
+        subscribeAvailableServerChange();
+
+        subscribeSessionStateChange();
+
         markAvailable();
-        tryToBeMaster();
     }
 
-    private void tryToBeMaster() {
-        isMaster = false;
-        try {
-            final List<String> availableServers = zkClientx.getChildren(availablePath);
-            if (CollectionUtils.isNotEmpty(availableServers)) {
-                final Stat stat = new Stat();
-                final String masterServer = zkClientx.readData(masterPath, stat);
-                if (!availableServers.contains(masterServer)) {
-                    zkClientx.writeData(masterPath, thisServer, stat.getVersion());
-                    isMaster = true;
+    private void subscribeSessionStateChange() {
+        zkClientx.subscribeStateChanges(new IZkStateListener() {
+            @Override
+            public void handleStateChanged(Watcher.Event.KeeperState state) throws Exception {
+                try {
+                    isMaster = false;
+                    if (state == Watcher.Event.KeeperState.SyncConnected) {
+                        markAvailable();
+                    }
+                } catch (Exception e) {
+                    log.error("handleStateChanged error", e);
                 }
             }
-        } catch (Exception e) {
-            log.error("tryToBeMaster error", e);
-        }
+
+            @Override
+            public void handleNewSession() throws Exception {
+                log.info("handleNewSession");
+            }
+
+            @Override
+            public void handleSessionEstablishmentError(Throwable error) throws Exception {
+                log.error("handleSessionEstablishmentError", error);
+                isMaster = false;
+            }
+        });
     }
+
+    private void subscribeAvailableServerChange() {
+        zkClientx.subscribeChildChanges(availablePath, (parentPath, availableServers) -> {
+            try {
+                if (CollectionUtils.isEmpty(availableServers)) {
+                    isMaster = false;
+                    markAvailable();
+                } else {
+                    final Stat oldMasterStat = new Stat();
+                    final String masterServer = zkClientx.readData(masterPath, oldMasterStat);
+                    if (availableServers.contains(masterServer)) {
+                        isMaster = StringUtils.equals(thisServer, masterServer);
+                    } else {
+                        zkClientx.writeData(masterPath, thisServer, oldMasterStat.getVersion());
+                        isMaster = true;
+                    }
+                }
+            } catch (Exception e) {
+                log.error("subscribeChildChanges error", e);
+            }
+        });
+    }
+
 
     private void tryToBuildZkFileStructure() {
         try {
